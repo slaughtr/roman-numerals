2d5f2e3f5e85edacec8c851dc1e7672e500a47f3
Chris Wainner and Dallas Slaughter <dallas@>
2017-02-23 09:48:48 -0800

    Add JS logic to convert numbers into roman numerals

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,50 @@
-var singleDigitArray = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+var singleDigitArray = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"];
+var tensDigitArray = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
 
-var singleDigit = function(input) {
-  onesPlace = singleDigitArray[input];
-  return onesPlace;
+var romanNumeralsArray = [
+  ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+  ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+  ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+  ["M", "MM", "MMM", "MV\u0305", "V\u0305", "V\u0305M", "V\u0305MM", "V\u0305MMM", "MX\u0305"]
+];
+
+var Convert = function(input) {
+  var result = [];
+  var splitInput = input.split("").reverse();
+  console.log(splitInput);
+
+  for(var index = 0; index < splitInput.length; index++){
+    if(splitInput[index] === "0"){
+      console.log("Found a zero: " + index);
+    } else{
+      var inputInt= parseInt(splitInput[index]);
+      result.unshift(romanNumeralsArray[index][inputInt-1]);
+      console.log(parseInt(splitInput[index]));
+      console.log(romanNumeralsArray[index][inputInt-1]);
+      // console.log(romanNumeralsArray[0][1]);
+    }
+  }
+
+
+  // onesPlace = singleDigitArray[input-1];
+  return result.join("");
 }
+
+// var tensDigit = function(input){
+//   tensPlace = tensDigitArray[input-1];
+//   return tensPlace;
+// }
 
 $(document).ready(function(){
 
   $("form#userInput").submit(function(){
+    console.log(romanNumeralsArray[3][3]);
     event.preventDefault();
     console.log("catching form submit");
-    var value = parseInt($("#inputString").val());
+    var value = $("#inputString").val();
     $("#result").show();
-    console.log(singleDigit(value));
-    $("#resultP").text(singleDigit(value));
+    console.log(Convert(value));
+    $("#resultP").text(Convert(value));
   });
 
 });
